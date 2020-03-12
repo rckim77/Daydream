8810ab294f361df546cea92d70a726820a680b76
rckim77 <rckim77@gmail.com>
2020-03-11 22:43:24 -0400

    adds app delegate

---
+++ Daydream/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Daydream
+//
+//  Created by Raymond Kim on 2/19/18.
+//  Copyright © 2018 Raymond Kim. All rights reserved.
+//
+
+import UIKit
+import GooglePlaces
+import GoogleMaps
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    static let googleAPIKey = "TEST"
+    static let yelpAPIKey = "TEST"
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        GMSPlacesClient.provideAPIKey(AppDelegate.googleAPIKey)
+        GMSServices.provideAPIKey(AppDelegate.googleAPIKey)
+        return true
+    }
+
+}
