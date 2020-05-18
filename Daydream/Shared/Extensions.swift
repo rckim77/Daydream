01eb8544ce006e74827d14433761e57d4009a2ed
rckim77 <rckim77@gmail.com>
2020-05-18 00:11:25 -0400

    removes unused Color file, extension

--- Daydream/Shared/Extensions.swift
+++ Daydream/Shared/Extensions.swift
@@ -51,31 +51,6 @@ extension GMSPlace: Placeable {
     }
 }
 
-extension UIColor {
-    convenience init(hexString: String) {
-        let hexString = hexString.trimmingCharacters(in: .whitespacesAndNewlines)
-        let scanner = Scanner(string: hexString)
-
-        if hexString.hasPrefix("#") {
-            scanner.scanLocation = 1
-        }
-
-        var color: UInt32 = 0
-        scanner.scanHexInt32(&color)
-
-        let mask = 0x000000FF
-        let r = Int(color >> 16) & mask
-        let g = Int(color >> 8) & mask
-        let b = Int(color) & mask
-
-        let red = CGFloat(r) / 255
-        let green = CGFloat(g) / 255
-        let blue = CGFloat(b) / 255
-
-        self.init(red: red, green: green, blue: blue, alpha: 1)
-    }
-}
-
 extension UISearchController {
     func setStyle() {
         // style cancel button
