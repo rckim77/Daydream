e6ac6c041d2d0c7bf066d1b456a59d9f8458d55c
rckim77 <rckim77@gmail.com>
2020-05-22 11:00:37 -0400

    updates Snapfile with modern devices, fixes snapshot UI test

--- DaydreamUITests/DaydreamUITests.swift
+++ DaydreamUITests/DaydreamUITests.swift
@@ -11,9 +11,50 @@ import XCTest
 class DaydreamUITests: XCTestCase {
 
     override func setUp() {
+        super.setUp()
         continueAfterFailure = false
+    }
+
+    func testUserInputCityAndSelectFirstAutocorrectTableViewCell() {
         let app = XCUIApplication()
+        app.launchEnvironment = ["isUITest": "true"]
         setupSnapshot(app)
         app.launch()
+
+        snapshot("01SearchVC")
+
+        let searchField = app.searchFields["e.g., Tokyo"]
+
+        searchField.tap()
+
+        sleep(2)
+
+        searchField.typeText("Tokyo")
+
+        snapshot("02AutocorrectVC")
+
+        app.tables["Search results"].staticTexts["Tokyo"].tap()
+
+        sleep(5)
+
+        snapshot("03SearchDetailVC")
+
+        app.tables.otherElements["poi1Card"].tap()
+
+        sleep(4)
+
+        snapshot("04MapVC")
+
+        app.buttons["mapCloseIconSoftShadow"].tap()
+
+        app.tables.otherElements["poi2Card"].tap()
+
+        sleep(2)
+
+        app.buttons["nightIcon"].tap()
+
+        sleep(5)
+
+        snapshot("05MapVCDark")
     }
 }
