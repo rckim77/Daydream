c14f7ca49ca98f6de986f73a34ba7bae22480163
rckim77 <rckim77@gmail.com>
2020-06-09 00:27:19 -0400

    fixes error image insets clipping bug

--- Daydream/Shared/Extensions.swift
+++ Daydream/Shared/Extensions.swift
@@ -183,7 +183,7 @@ extension UIButton {
         tintColor = .white
         backgroundColor = UIColor.black.withAlphaComponent(0.2)
         setImage(retryImage, for: .normal)
-        imageEdgeInsets.right = 18
+        imageEdgeInsets.right = 12
         addRoundedCorners(radius: 8)
     }
 }
