145be4c7cadcfdfbb2cf4391041a83095dcf8e35
Ray Kim <rckim77@gmail.com>
2025-12-11 19:17:44 -0500

    uses mutex for image and places caches

--- Daydream/Shared/ImageCache.swift
+++ Daydream/Shared/ImageCache.swift
@@ -6,25 +6,35 @@
 //  Copyright © 2024 Raymond Kim. All rights reserved.
 //
 
+import Synchronization
 import UIKit
 
 /// Uses `Photo` hashValue as hash key (converted from `Int` to `String`).
+/// The cache uses a `Mutex` primitive to ensure safe access to the cache across
+/// threads without the overhead of an actor. Whereas an actor shines when adopting
+/// an asynchronous process, mutexes work better for synchronous, immediate access.
 final class ImageCache {
     static let shared = ImageCache()
     
-    private let cache = NSCache<NSString, UIImage>()
+    private let cache = Mutex<NSCache<NSString, UIImage>>(NSCache<NSString, UIImage>())
     
     private init() {}
     
     func set(_ image: UIImage, forKey key: String) {
-        cache.setObject(image, forKey: key as NSString)
+        cache.withLock { cache in
+            cache.setObject(image, forKey: key as NSString)
+        }
     }
     
     func get(forKey key: String) -> UIImage? {
-        return cache.object(forKey: key as NSString)
+        cache.withLock { cache in
+            return cache.object(forKey: key as NSString)
+        }
     }
     
     func clear() -> Void {
-        cache.removeAllObjects()
+        cache.withLock { cache in
+            cache.removeAllObjects()
+        }
     }
 }
